7ab9eeee569ee517d15332ed2d2853bd780c5519
Erinome <rand256@yandex.ru>
2020-06-01 09:24:08 +0300

    do always use geometry polyfill

--- client/js/geometry-polyfill.js
+++ client/js/geometry-polyfill.js
@@ -68,7 +68,7 @@
 	  }
 	}
 
-	window.DOMPoint = window.DOMPoint || DOMPoint;
+	window.DOMPoint = DOMPoint;
   }
 
   // @info
@@ -106,7 +106,7 @@
 	  }
 	}
 
-	window.DOMRect = window.DOMRect || DOMRect;
+	window.DOMRect = DOMRect;
   }
 
   // @info
@@ -711,5 +711,5 @@
 	  }
 	}
 
-	window.DOMMatrix = window.DOMMatrix || DOMMatrix;
+	window.DOMMatrix = DOMMatrix;
   }
